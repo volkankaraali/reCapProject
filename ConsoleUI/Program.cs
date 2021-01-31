0094caa7a464588b1804b03ea217011d6b74fd0c
volkan karaali <volkankaraali1@gmail.com>
2021-01-31 22:15:36 +0300

    recapproject first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,59 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Abstract;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Console.WriteLine("*****Tüm veriler********");
+            foreach (var car in carManager.GetAll())
+            { 
+                Console.WriteLine("id:{0} --- aciklama:{1}",car.Id,car.Description);
+            }
+
+            Car car1 = new Car() {Id=9,BrandId=2,ColorId=3,DailyPrice=1234567,ModelYear=2009,Description="none"};
+            carManager.Add(car1);
+
+            Console.WriteLine("\n***** Add metodu ile eklenen veri GetById ile çekildi*******");
+            foreach (var i in carManager.GetById(9))
+            {
+                Console.WriteLine("id:{0}\nmarkaId:{1}\nrenkId:{2}\nfiyati:{3}\nmodeli:{4}\naciklamasi:{5}",i.Id,i.BrandId,i.ColorId,i.DailyPrice,i.ModelYear,i.Description);
+            }
+
+            Console.WriteLine("\n***** Update metodu ile güncellenen veri******");
+            Console.WriteLine("önceki veri;");
+            foreach (var i in carManager.GetById(9))
+            {
+                Console.WriteLine("id:{0}\nmarkaId:{1}\nrenkId:{2}\nfiyati:{3}\nmodeli:{4}\naciklamasi:{5}", i.Id, i.BrandId, i.ColorId, i.DailyPrice, i.ModelYear, i.Description);
+            }
+
+            Car carReNew = new Car();
+            carReNew.Id = 9;
+            carReNew.BrandId = 2;
+            carReNew.ColorId = 3;
+            carReNew.DailyPrice = 1234567;
+            carReNew.ModelYear = 2009;
+            carReNew.Description = "Fiat-Doblo";
+
+            carManager.Update(carReNew);
+            Console.WriteLine("\ngüncellenmiş veri;");
+            foreach (var i in carManager.GetById(9))
+            {
+                Console.WriteLine("id:{0}\nmarkaId:{1}\nrenkId:{2}\nfiyati:{3}\nmodeli:{4}\naciklamasi:{5}", i.Id, i.BrandId, i.ColorId, i.DailyPrice, i.ModelYear, i.Description);
+            }
+
+            Console.WriteLine("\n******Delete metodu ile silinen id=1 olan veri silindi sonraki veriler*******");
+            carManager.Delete(new Car() {Id=1 });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("id:{0} --- aciklama:{1}", car.Id, car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2018,DailyPrice=500000,Description="Audi"},
+                new Car{Id=2,BrandId=1,ColorId=1,ModelYear=2015,DailyPrice=400000,Description="Audi"},
+                new Car{Id=3,BrandId=2,ColorId=1,ModelYear=2010,DailyPrice=100000,Description="Opel"},
+                new Car{Id=4,BrandId=2,ColorId=1,ModelYear=2009,DailyPrice=95000,Description="Opel"},
+                new Car{Id=5,BrandId=3,ColorId=1,ModelYear=2000,DailyPrice=50000,Description="Fiat"},
+                new Car{Id=6,BrandId=4,ColorId=1,ModelYear=2020,DailyPrice=1000000,Description="BMW"},
+                new Car{Id=7,BrandId=4,ColorId=1,ModelYear=2007,DailyPrice=100000,Description="BMW"},
+                new Car{Id=8,BrandId=3,ColorId=1,ModelYear=2012,DailyPrice=120000,Description="Fiat"},
+            };
+        }
+        public void Add(Car car)
+        {
+            
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            //linq used for to delete 
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.Id==carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
